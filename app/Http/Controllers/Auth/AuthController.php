bd3f2f66877628464382b5488fed3008e1151f29
Suu2 <46to90@gmail.com>
2017-04-20 13:47:50 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,11 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    // 追加 
+    protected $redirectTo = '/';
+    protected $loginPath = '/login';    // 追加
+
+
     /**
      * Create a new authentication controller instance.
      *
